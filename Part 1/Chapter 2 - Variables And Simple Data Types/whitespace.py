a0ebb1d77a430d04d07c0be3e9ccba0a661b025a
lonewolfcub <lonewolfcub020@gmail.com>
2017-07-21 08:32:43 +0100

    Initial save

---
+++ Part 1/Chapter 2 - Variables And Simple Data Types/whitespace.py
@@ -0,0 +1,17 @@
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavascript")
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+
+favourite_language = 'python  '
+print(favourite_language)
+print(favourite_language.rstrip())
+print(favourite_language)
+
+favourite_language = favourite_language.rstrip()
+print(favourite_language)
+
+favourite_language = '   python   '
+print(favourite_language.rstrip())
+print(favourite_language.lstrip())
+print(favourite_language.strip())
