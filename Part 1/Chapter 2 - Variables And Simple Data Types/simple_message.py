223660b82b5c95a934931dd852f84976470745c1
lonewolfcub <lonewolfcub020@gmail.com>
2017-07-21 08:06:23 +0100

    Initial save

---
+++ Part 1/Chapter 2 - Variables And Simple Data Types/simple_message.py
@@ -0,0 +1,2 @@
+message = ("This is a message for the robots of Earth!")
+print(message)
---
+++ Part 1/Chapter 2 - Variables And Simple Data Types/simple_messages.py
@@ -0,0 +1,4 @@
+message = "This is a message for the robots of Earth!"
+print(message)
+message = "Kill all humans!"
+print(message)
