de39b9acd37893d2d4636eef888c4a9a9e6f2f5b
lonewolfcub <lonewolfcub020@gmail.com>
2017-07-28 07:25:30 +0100

    Initial save

---
+++ Part 1/Chapter 3 - Introducing Lists/3-10_every_function.py
@@ -0,0 +1,61 @@
+gen1 = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon",
+        "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie",
+        "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey",
+        "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow",
+        "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash",
+        "Nidoran(f)", "Nidorina", "Nidoqueen", "Nidoran(m)", "Nidorino",
+        "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales",
+        "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom",
+        "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett",
+        "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey",
+        "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl",
+        "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke",
+        "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool",
+        "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash",
+        "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd",
+        "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder",
+        "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee",
+        "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute",
+        "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan",
+        "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey",
+        "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking",
+        "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz",
+        "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras",
+        "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon",
+        "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax",
+        "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite",
+        "Mewtwo", "Mew"]
+
+# Find the length of a list
+print("There are" + str(len(gen1) + "pokemon in the Kanto Pokedex")
+
+# Print list
+print("I've stored them in the following list")
+print(gen1)
+
+# Print a list in reverse order
+print("To reverse the order of the pokedex and print the value, use the .reverse method")
+gen1.reverse()
+print(gen1)
+print("To restore the original order, simply reverse again")
+gen1.reverse()
+print(gen1)
+
+# Sort a list temporarily using sorted()
+
+# Temporarily reverse sort using sorted()
+
+# Print element 0 in list
+# Print element 0 in list in titlecase
+# Print non 0 element in list
+# Print last element in list
+# Concatenate a list value into a string
+# Change the value of an element
+# Append element to string
+# Insert element into string
+# Remove element from string using delete statement
+# Remove element 0 using pop statement
+# Remove a non 0 element using pop statement
+# Remove an item by value
+# Sort a list permanently using sort()
+# Reverse sort a list permanently using sort()
