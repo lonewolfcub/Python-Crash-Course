c5eb2371c82faf8a5b1f3b003639984c73e72f93
lonewolfcub <lonewolfcub020@gmail.com>
2017-07-28 15:31:41 +0100

    Initial save

---
+++ Part 1/Chapter 5 - If Statements/5-1_conditional_tests.py
@@ -0,0 +1,41 @@
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+animal = 'cat'
+print("\nIs animal == 'cat'? I predict True.")
+print(animal == 'cat')
+
+print("\nIs animal == 'dog'? I predict False.")
+print(animal == 'dog')
+
+fruit = 'apple'
+print("\nIs fruit == 'apple'? I predict True.")
+print(fruit == 'apple')
+
+print("\nIs fruit == 'banana'? I predict False.")
+print(fruit == 'banana')
+
+exercise = 'tedious'
+print("\nIs exercise  == 'tedious'? I predict True.")
+print(exercise == 'tedious')
+
+print("\nIs exercise == 'useful'? I predict False.")
+print(exercise == 'useful')
+
+day = 'friday'
+print("\nIs day == 'friday'? I predict True.")
+print(day == 'friday')
+
+print("\nIs day == 'saturday'? I predict False.")
+print(day == 'saturday')
+
+meaning_of_life = 42
+print("\nIs meaning_of_life == 42? I predict True.")
+print(meaning_of_life == 42)
+
+print("\nIs meaning_of_life == 'live fast, die young'? I predict False.")
+print(meaning_of_life == 'live fast, die young')
---
+++ Part 1/Chapter 5 - If Statements/5-2_more_conditional_tests.py
@@ -0,0 +1,58 @@
+# Test for equality with strings
+print('Does "apples" == "apples?')
+fruit = 'apples'
+print('apples' == fruit)
+
+# Test for inequality with strings
+print('\nDo "apples" != "oranges?"')
+fruit = 'apples'
+print('oranges' != fruit)
+
+# Test using the lower() method
+print('\nDoes "Apples" == "apples"?')
+fruit = 'apples'
+print('Apples' == fruit)
+print('\nDoes "Apples".lower() == "apples"?')
+print('Apples'.lower() == fruit)
+
+# Numerical test for equality
+print("\nDoes 2 + 2 == 4?")
+print(2 + 2 == 4)
+
+# Numerical test for inequality
+print("\nDoes 2 + 2 != 5?")
+print(2 + 2 != 5)
+
+# Numerical test for greater than
+print("\nIs 2 > 1?")
+print(2 > 1)
+
+# Numerical test for less than
+print("\nIs 1 < 2?")
+print(1 < 2)
+
+# Numerical test for greater than or equal to
+print("\nIs 2 + 2 >= 4?")
+print(2 + 2 >= 4)
+
+# Numerical test for less than or equal to
+print("\nIs 2 + 2 <= 5?")
+print(2 + 2 <= 5)
+
+# Test using the and keyword
+print("\nAre 2 + 2 and 2 + 3 > 3?")
+print((2 + 2) and (2 + 3) >3 )
+
+# Test using the or keyword
+print("\nIs 4 == 2 + 2 or 2 + 3")
+print(4 == (2 + 2) or (2 + 3))
+
+# Test whether an item is in a list
+cool_kids = ["mike", "tyler", "joey", "trey"]
+print("\nIs joey in cool_kids?")
+print("joey" in cool_kids)
+
+# Test whether an item is not in a list
+cool_kids = ["mike", "tyler", "joey", "trey"]
+print("\nIs Sebastian not in cool_kids?")
+print("sebastian" not in cool_kids)
