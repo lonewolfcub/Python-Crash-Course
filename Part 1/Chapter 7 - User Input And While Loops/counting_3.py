6c850faad86a0fee674bf5060f6a391dee5df60e
lonewolfcub <lonewolfcub020@gmail.com>
2017-08-11 15:01:55 +0100

    Initial save

---
+++ Part 1/Chapter 7 - User Input And While Loops/counting_3.py
@@ -0,0 +1,11 @@
+x = 1
+while x <= 5:
+    print(x)
+    x += 1
+
+# the code commented out below is an example of an infinite loop
+#x = 1
+#while x <= 5:
+#    x += 1
+
+# If you write and infinite loop, press CTRL-C to force quit the program
