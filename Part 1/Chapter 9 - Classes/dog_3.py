3ffbc7b6c42f2cc5bfd412e2e505b89e8c8a2a51
lonewolfcub <lonewolfcub020@gmail.com>
2017-08-18 14:35:33 +0100

    Initial save

---
+++ Part 1/Chapter 9 - Classes/dog_3.py
@@ -0,0 +1,20 @@
+class Dog():
+    """A simple attempt to model a dog."""
+
+    def __init__(self, name, age):
+        """Initialise name and age attributes."""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        """Simulate rolling over in response to a commmand."""
+        print(self.name.title() + " rolled over!")
+
+my_dog = Dog('willie', 6)
+
+my_dog.sit()
+my_dog.roll_over()
