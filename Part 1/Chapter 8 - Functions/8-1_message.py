910063b90aa3548e952f1640765ec2a4605437e9
lonewolfcub <lonewolfcub020@gmail.com>
2017-08-18 07:53:18 +0100

    Initial save

---
+++ Part 1/Chapter 8 - Functions/8-1_message.py
@@ -0,0 +1,5 @@
+def display_message():
+    """Inform users of today's topic of study"""
+    print("Today we are learning about functions!")
+
+display_message()
---
+++ Part 1/Chapter 8 - Functions/8-2_favourite_book.py
@@ -0,0 +1,4 @@
+def favourite_book(title):
+    print("One of my favourite books is " + title + ".")
+
+favourite_book("The Hobbit")
