4560e26a3fb2ea9f0c1664f7536be9f769110cb9
lonewolfcub <lonewolfcub020@gmail.com>
2017-08-18 08:08:26 +0100

    Initial save

---
+++ Part 1/Chapter 8 - Functions/pets_4.py
@@ -0,0 +1,8 @@
+def describe_pet(pet_name, animal_type='dog'):
+    """Display information about a pet."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+describe_pet('willie')
+
+describe_pet(pet_name='Harry', animal_type='hamster')
