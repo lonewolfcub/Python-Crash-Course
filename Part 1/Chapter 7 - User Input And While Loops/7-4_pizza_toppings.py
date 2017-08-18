cece8a90017a823fb39be28684991c878159db28
lonewolfcub <lonewolfcub020@gmail.com>
2017-08-18 06:13:48 +0100

    Initial save

---
+++ Part 1/Chapter 7 - User Input And While Loops/7-4_pizza_toppings.py
@@ -0,0 +1,12 @@
+prompt = "What toppings would you like you pizza?: "
+prompt += "\n "
+
+active = True
+while active:
+    topping = input(prompt)
+
+    if topping == 'quit':
+        active = False
+
+    else:
+        print("I'll add " + topping + " to your pizza!")
