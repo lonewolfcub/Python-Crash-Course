319817e0860dc235ea530befee72ace587e7abb5
lonewolfcub <lonewolfcub020@gmail.com>
2017-08-18 13:54:46 +0100

    Initial save

--- Part 1/Chapter 8 - Functions/making_pizzas.py
+++ Part 1/Chapter 8 - Functions/making_pizzas.py
@@ -24,6 +24,16 @@ mp(12, 'mushrooms', 'green peppers', 'extra cheese')
 # after importing, the function is available to call without any using the
 # specified alias
 
+# to assign an alias to a module, use the syntax
+
+import pizza_4 as p
+
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# after importing, any function in the module can be called by using the
+# alias and then the module name
+
 
 # to import all functions in a module, use the syntax
 from pizza_4 import *
