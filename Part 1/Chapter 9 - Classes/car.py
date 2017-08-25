66f51f2c0681571aa79297595936eb7ad3bc664c
lonewolfcub <lonewolfcub020@gmail.com>
2017-08-25 06:34:05 +0100

    Fixes for method documentation format

--- Part 1/Chapter 9 - Classes/car.py
+++ Part 1/Chapter 9 - Classes/car.py
@@ -1,14 +1,14 @@
 class Car():
-    ###A simple attempt to represent a car.###
+    """A simple attempt to represent a car."""
 
     def __init__(self, make, model, year):
-        ###Initialise attributes to describe a car###
+        """Initialise attributes to describe a car"""
         self.make = make
         self.model = model
         self.year = year
 
     def get_descriptive_name(self):
-        ###Return a neatly formatted descriptive name.###
+        """Return a neatly formatted descriptive name."""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
 
--- Part 1/Chapter 9 - Classes/car_5.py
+++ Part 1/Chapter 9 - Classes/car_5.py
@@ -1,25 +1,27 @@
 class Car():
-    ###A simple attempt to represent a car.###
+    """A simple attempt to represent a car."""
 
     def __init__(self, make, model, year):
-        ###Initialise attributes to describe a car###
+        """Initialise attributes to describe a car"""
         self.make = make
         self.model = model
         self.year = year
         self.odometer_reading = 0
 
     def get_descriptive_name(self):
-        ###Return a neatly formatted descriptive name.###
+        """Return a neatly formatted descriptive name."""
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
 
     def read_odometer(self):
-        ###Print a statement showing the car's milage.###
+        """Print a statement showing the car's milage."""
         print("This car has " + str(self.odometer_reading) + " miles on it.")
 
     def update_odometer(self, milage):
-        ### Set the odometer reading to the given value.###
-        ###Reject the change if it attempts to roll the odometer back.###
+        """ 
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
         if milage >= self.odometer_reading:
             self.odometer_reading = milage
         else:
