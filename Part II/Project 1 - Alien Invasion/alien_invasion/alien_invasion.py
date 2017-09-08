3d71a12b5d4e4c0a3fef1184ce58c4f065b8e4fb
lonewolfcub <lonewolfcub020@gmail.com>
2017-09-08 08:02:19 +0100

    Update

--- Part II/Project 1 - Alien Invasion/alien_invasion/alien_invasion.py
+++ Part II/Project 1 - Alien Invasion/alien_invasion/alien_invasion.py
@@ -14,7 +14,7 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship.
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Set the background color.
     bg_color = (230, 230, 230)
@@ -22,7 +22,8 @@ def run_game():
     # Start the main loop for the game
     while True:
 
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 run_game()
