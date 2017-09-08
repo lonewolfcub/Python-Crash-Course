eb6ab2ae40e76c311c9755d42f3f141972d0101b
lonewolfcub <lonewolfcub020@gmail.com>
2017-09-08 08:04:41 +0100

    Update

--- Part II/Project 1 - Alien Invasion/alien_invasion/settings.py
+++ Part II/Project 1 - Alien Invasion/alien_invasion/settings.py
@@ -3,10 +3,16 @@ class Settings():
 
     def __init__(self):
         """Initialize the game's settings."""
-        #Screen settings
+        # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-        #Ship Settings
+        # Ship Settings
         self.ship_speed_factor = 1.5
+
+        # Bullet Settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
