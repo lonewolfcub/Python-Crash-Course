cbd236a9c5e6e5b5c96501076efdb1e5b3bd64be
lonewolfcub <lonewolfcub020@gmail.com>
2017-09-22 08:18:01 +0100

    Initial Commit

---
+++ Part II/Project 2 - Data Visualisation/Chapter 15 - Generating Data/rw_visual_3.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+from random_walk_2 import RandomWalk
+
+# Keep making new walks, as long as the program is active.
+while True:
+    # Make a random walk, and plot the points.
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+                edgecolor='none', s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
