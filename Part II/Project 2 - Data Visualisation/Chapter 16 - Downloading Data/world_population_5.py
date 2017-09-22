aee51137128ef7fad3c4cb2b32b9911fccf45c9f
lonewolfcub <lonewolfcub020@gmail.com>
2017-09-22 16:46:20 +0100

    Initial Commit

---
+++ Part II/Project 2 - Data Visualisation/Chapter 16 - Downloading Data/world_population_5.py
@@ -0,0 +1,23 @@
+import json
+from pygal.maps.world import World
+from country_codes_2 import get_country_code
+
+# Load the data into a list.
+filename = 'population_data.json'
+with open(filename) as f:
+    pop_data = json.load(f)
+
+# Build a dictionary of population data.
+cc_populations = {}
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2010':
+        country = pop_dict['Country Name']
+        population = int(float(pop_dict['Value']))
+        code = get_country_code(country)
+        if code:
+            cc_populations[code] = population
+
+wm = World
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+wm.render_to_file('world_population_5.svg')
