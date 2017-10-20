a88e2d699b1254728257905025c15f1387d03013
lonewolfcub <lonewolfcub020@gmail.com>
2017-10-20 14:00:47 +0100

    Initial Commit

--- Part-II/Project-3-Web-Applications/learning_log/learning_log/settings.py
+++ Part-II/Project-3-Web-Applications/learning_log/learning_log/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # My apps
-    'learning_logs'
+    'learning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# My settings
+LOGIN_URL = '/users/login/'
---
+++ Part-II/Project-3-Web-Applications/learning_log/users/urls.py
@@ -0,0 +1,18 @@
+"""Defines URL patterns for users"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import login
+
+from . import views
+
+urlpatterns = [
+    # Login page
+    url(r'^login/$', login, {'template_name': 'users/login.html'},
+        name='login'),
+
+    # Log Out Page
+    url(r'^logout/$', views.logout_view, name='logout'),
+
+    # Registration Page
+    url(r'^register/$', views.register, name='register')
+]
