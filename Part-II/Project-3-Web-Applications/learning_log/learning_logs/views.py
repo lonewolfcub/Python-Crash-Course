3f7056216a145b8e3d1d75cea30e245d01a3b94a
lonewolfcub <lonewolfcub020@gmail.com>
2017-10-27 10:23:05 +0100

    Initial Commit

--- Part-II/Project-3-Web-Applications/learning_log/learning_logs/views.py
+++ Part-II/Project-3-Web-Applications/learning_log/learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 
@@ -23,6 +23,9 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all it's entries."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    check_topic_owner(topic.owner, request.user)
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -37,7 +40,9 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
 
     context = {'form': form}
@@ -47,6 +52,8 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    check_topic_owner(topic.owner, request.user)
 
     if request.method != 'POST':
         # No data submitted; create a blank form.
@@ -69,6 +76,7 @@ def edit_entry(request, entry_id):
     """Edit an exisiting entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(topic.owner, request.user)
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry
@@ -83,3 +91,7 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+def check_topic_owner(topic_owner, request_user):
+    if topic_owner != request_user:
+        raise Http404
---
+++ Part-II/Project-3-Web-Applications/learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Trevor/PycharmProjects/Python-Crash-Course/Python-Crash-Course/Part-II/Project-3-Web-Applications/learning_log/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
